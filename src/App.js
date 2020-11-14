0614b3ca55f37cd354b55331882e88c07b7f63fd
mostafa <mostafaragab373@gmail.com>
2020-11-14 19:44:14 +0200

    fixing bugs in menu-item and app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
-import Header from "./components/menu-item/header/header";
+import Header from "./components/header/header";
 import HomePage from "./pages/homepage";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-sign-up";
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<HomePage />
+			<Switch>
+				<Route exact path="/" component={HomePage} />
+				<Route exact path="/sign-in" component={SignInAndSignUp} />
+			</Switch>
 		</div>
 	);
 }
