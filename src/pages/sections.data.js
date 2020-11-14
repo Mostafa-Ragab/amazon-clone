ae5555966f85f16641c6b60ab69411d92b40df34
mostafa <mostafaragab373@gmail.com>
2020-11-14 15:56:22 +0200

    adding data into home page

---
+++ src/pages/sections.data.js
@@ -0,0 +1,58 @@
+const sections = [
+	{
+		title: "Samsung Galaxy S20 Ultra (Cosmic Gray, 12GB RAM, 128GB Storage)",
+		price: 97999.0,
+		imageUrl:
+			"https://images-na.ssl-images-amazon.com/images/I/71cD4NUIBWL._SY550_.jpg", // local file in public/images/
+		rating: 4,
+		id: 1,
+	},
+	{
+		title:
+			"Acer Aspire 3 A315-56 15.6-inch Laptop (Intel Core i5-1035G1/8GB/1TB HDD/Window 10, Home, 64Bit/Intel UHD Graphics), Black",
+		// imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+		price: 97999.0,
+		imageUrl:
+			"https://images-na.ssl-images-amazon.com/images/I/810mRHoIxML._SX466_.jpg", // local file in public/images/
+		rating: 4,
+		id: 2,
+	},
+	{
+		title: "Mi Earphone Basic with Ultra deep bass and mic (Black)",
+		price: 3435,
+		imageUrl:
+			"https://images-na.ssl-images-amazon.com/images/I/71mQ9oKDMLL._SX522_.jpg", // local file in public/images/
+		rating: 3,
+		id: 3,
+		size: "medium",
+	},
+	{
+		title: "React.js Essentials Paperback – Import, 27 August 2015",
+		price: 1435,
+		imageUrl:
+			"https://images-na.ssl-images-amazon.com/images/I/41-HWOw--cL._SX404_BO1,204,203,200_.jpg", // local file in public/images/
+		rating: 3,
+		id: 4,
+		size: "medium",
+	},
+	{
+		title: "I Too Had a Love Story Paperback – 6 November 2013",
+		price: 8435,
+		imageUrl:
+			"https://images-na.ssl-images-amazon.com/images/I/51JdOfIHZ9L._SX324_BO1,204,203,200_.jpg", // local file in public/images/
+		rating: 5,
+		id: 5,
+		size: "medium",
+	},
+	{
+		title:
+			"Sony Bravia 138.8 cm (55 inches) 4K Ultra HD Smart LED TV KD-55X7002G (Black)",
+		price: 8435,
+		imageUrl:
+			"https://images-na.ssl-images-amazon.com/images/I/81Nw2ifyBzL._SX569_.jpg", // local file in public/images/
+		rating: 2,
+		id: 6,
+	},
+];
+
+export default sections;
