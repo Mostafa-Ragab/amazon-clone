bf0f30345843b5426b4fd914c5460d251b5f10e3
mostafa <mostafaragab373@gmail.com>
2020-11-14 18:21:49 +0200

    adding header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import Header from "./components/menu-item/header/header";
 import HomePage from "./pages/homepage";
 function App() {
 	return (
 		<div className="App">
+			<Header />
 			<HomePage />
 		</div>
 	);
