157ca47459746d925d1b1691b58f906c0051ef6b
mostafa <mostafaragab373@gmail.com>
2020-11-14 15:08:59 +0200

    adding menuitems component

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,23 +1,27 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+
 import {
 	Title,
 	Container,
 	Price,
-	Button,
 	Image,
 	Rating,
+	Info,
 } from "./menu-item.styles";
-const MenuItem = ({ title, rating, imageUrl, price }) => (
-	<Container>
-		<Title>{title}</Title>
-		<Price>${price}</Price>
-		<Rating>
-			{Array(rating)
-				.fill()
-				.map((_) => (
-					<p>⭐</p>
-				))}
-		</Rating>
+const MenuItem = ({ size, title, rating, imageUrl, price }) => (
+	<Container className={`${size}`}>
+		<Info>
+			<Title>{title}</Title>
+			<Price>${price}</Price>
+			<Rating>
+				{Array(rating)
+					.fill()
+					.map((_) => (
+						<p>⭐</p>
+					))}
+			</Rating>
+		</Info>
 		<Image src={imageUrl} alt="item" />
 		<Button>ADD TO BASKET</Button>
 	</Container>
