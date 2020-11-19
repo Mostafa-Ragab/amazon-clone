abeeb5845ee2c0bcfb4e7d2e102c8f42cc254d15
mostafa <mostafaragab373@gmail.com>
2020-11-19 19:05:50 +0200

    adding header into home pgae

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -2,20 +2,24 @@ import React from "react";
 import "./home.css";
 import sections from "./sections.data";
 import MenuItem from "../components/menu-item/menu-item";
+import Header from "../components/header/header";
 
 const HomePage = () => (
-	<div className="home">
-		<img
-			className="home__image"
-			src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
-			alt=""
-		/>
-		<div className="product">
-			{sections.map(({ id, ...restProps }) => (
-				<MenuItem key={id} {...restProps} />
-			))}
+	<>
+		<Header />
+		<div className="home">
+			<img
+				className="home__image"
+				src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+				alt=""
+			/>
+			<div className="product">
+				{sections.map(({ id, ...restProps }) => (
+					<MenuItem key={id} {...restProps} />
+				))}
+			</div>
 		</div>
-	</div>
+	</>
 );
 
 export default HomePage;
