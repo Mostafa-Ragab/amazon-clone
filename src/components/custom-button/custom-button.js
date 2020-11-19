29d5a337657f78e22ac9d8f00f722dea78ff9835
mostafa <mostafaragab373@gmail.com>
2020-11-19 16:46:17 +0200

    adding reusable buttons

---
+++ src/components/custom-button/custom-button.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./cutsom-button.css";
+
+const CustomButton = ({ children }) => (
+	<button className="custom-button">{children}</button>
+);
+
+export default CustomButton;
