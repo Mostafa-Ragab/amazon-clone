094c2c783ec12f7a529d42c6579b46df39264738
mostafa <mostafaragab373@gmail.com>
2020-11-19 19:32:14 +0200

    adding sign up

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage";
 import SignIn from "./pages/sign-in/sign-in";
+import SignUp from "./pages/sign-up/sign-up";
 function App() {
 	return (
 		<div className="App">
 			<Switch>
 				<Route exact path="/" component={HomePage} />
 				<Route exact path="/signin" component={SignIn} />
+				<Route exact path="/signup" component={SignUp} />
 			</Switch>
 		</div>
 	);
