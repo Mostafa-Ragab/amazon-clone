4f2ace7ae055a97a60ba9f06db1cd26f6e0bca5c
mostafa <mostafaragab373@gmail.com>
2020-11-25 10:28:07 +0200

    finishing sign up

--- src/pages/sign-up/sign-up.js
+++ src/pages/sign-up/sign-up.js
@@ -2,29 +2,46 @@ import React, { useState } from "react";
 import FormInput from "../../components/form-input/form-input";
 import "./sign-up.css";
 import { Link } from "react-router-dom";
-import { auth } from "../../components/firebase/firebase";
+import { auth, createUserProfileDocument } from "../../firebase/firebase";
+// import { auth } from "../../components/firebase/firebase";
 const SignUp = () => {
-	const [useCredentials, setCredentials] = useState({
-		name: "",
+	const [userCredentials, setCredentials] = useState({
+		displayName: "",
 		email: "",
 		password: "",
+		confirmPassword: "",
 	});
-	const { name, email, password } = useCredentials;
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
-		auth
-			.createUserWithEmailAndPassword(email, password)
-			.then((auth) => {
-				history.push("/");
-			})
-			.catch((e) => alert(e.message));
+		const { displayName, email, password, confirmPassword } = userCredentials;
+		if (password !== confirmPassword) {
+			alert("passwords don't match");
+			return;
+		}
+		try {
+			const { user } = await auth.createUserWithEmailAndPassword(
+				email,
+				password
+			);
+			await createUserProfileDocument(user, { displayName });
+			setCredentials({
+				displayName: "",
+				email: "",
+				password: "",
+				confirmPassword: "",
+			});
+		} catch (error) {
+			console.log(error);
+		}
 	};
 	const handleChange = (event) => {
 		const { value, name } = event.target;
 
-		setCredentials({ ...useCredentials, [name]: value });
+		setCredentials({ ...userCredentials, [name]: value });
 	};
+	const { displayName, email, password, confirmPassword } = userCredentials;
+
 	return (
 		<div className="sign-up">
 			<Link to="/">
@@ -38,11 +55,11 @@ const SignUp = () => {
 				<h1>Sign Up</h1>
 				<h4>Name</h4>
 				<FormInput
-					name="name"
-					type="name"
-					value={name}
-					placeholder="name"
-					handleChange={handleChange}
+					name="displayName"
+					type="text"
+					value={displayName}
+					onChange={handleChange}
+					label="Display Name"
 					required
 				/>
 				<h4>E-mail</h4>
@@ -51,7 +68,7 @@ const SignUp = () => {
 					type="email"
 					value={email}
 					placeholder="email"
-					handleChange={handleChange}
+					onChange={handleChange}
 					required
 				/>
 				<h4>Password</h4>
@@ -59,8 +76,17 @@ const SignUp = () => {
 					name="password"
 					type="password"
 					placeholder="password"
-					handleChange={handleChange}
+					onChange={handleChange}
 					value={password}
+					required
+				/>
+				<FormInput
+					name="confirmPassword"
+					type="password"
+					placeholder="confirm password"
+					onChange={handleChange}
+					value={confirmPassword}
+					label="Confirm Password"
 					required
 				/>
 				<button className="register" type="submit">
