6d2fe8eb66c8cd7546148bbc9bfd11b83d9247dc
mostafa <mostafaragab373@gmail.com>
2020-11-25 11:45:23 +0200

    passign current user state to header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./App.css";
 import HomePage from "./pages/homepage";
 import SignIn from "./pages/sign-in/sign-in";
 import SignUp from "./pages/sign-up/sign-up";
+import { auth, createUserProfileDocument } from "./firebase/firebase";
+
 class App extends Component {
 	constructor() {
 		super();
@@ -12,7 +14,31 @@ class App extends Component {
 			currentUser: null,
 		};
 	}
+	unsubscribeFromAuth = null;
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
 
+				userRef.onSnapshot((snapShot) => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data(),
+						},
+					});
+
+					console.log(this.state);
+				});
+			}
+
+			this.setState({ currentUser: userAuth });
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
 	render() {
 		return (
 			<div className="App">
@@ -20,8 +46,7 @@ class App extends Component {
 					<Route
 						exact
 						path="/"
-						component={HomePage}
-						currentUser={this.state.currentUser}
+						component={(_) => <HomePage currentUser={this.state.currentUser} />}
 					/>
 					<Route exact path="/signin" component={SignIn} />
 					<Route exact path="/signup" component={SignUp} />
