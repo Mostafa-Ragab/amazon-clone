ae63496e42c123c2e8286da26e82d0619bcce79f
mostafa <mostafaragab373@gmail.com>
2020-11-25 10:29:54 +0200

    adding current user state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage";
 import SignIn from "./pages/sign-in/sign-in";
 import SignUp from "./pages/sign-up/sign-up";
-function App() {
-	return (
-		<div className="App">
-			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route exact path="/signin" component={SignIn} />
-				<Route exact path="/signup" component={SignUp} />
-			</Switch>
-		</div>
-	);
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			currentUser: null,
+		};
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Switch>
+					<Route
+						exact
+						path="/"
+						component={HomePage}
+						currentUser={this.state.currentUser}
+					/>
+					<Route exact path="/signin" component={SignIn} />
+					<Route exact path="/signup" component={SignUp} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
