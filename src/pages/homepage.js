7ec5a328f7bad841a537fa8c5880ba3fd3848827
mostafa <mostafaragab373@gmail.com>
2020-11-25 11:45:41 +0200

    adding header inside home page

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -6,8 +6,8 @@ import Header from "../components/header/header";
 
 const HomePage = ({ currentUser }) => (
 	<>
-		<Header currentUser={currentUser} />
 		<div className="home">
+			<Header currentUser={currentUser} />
 			<img
 				className="home__image"
 				src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
