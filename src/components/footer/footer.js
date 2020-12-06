843787ba282f0b4ffb08a1b1662f841824fb6f4a
mostafa <mostafaragab373@gmail.com>
2020-12-06 15:28:43 +0200

    adding footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => (
+	<div className="footer">
+		<h2 className="title">Questions? Contact us.</h2>
+		<div className="row">
+			<div className="footer-column">
+				<h3 className="footer-title">Get to Know Us</h3>
+				<p>Careers</p>
+				<p>Amazon and Our Planet</p>
+				<p>Investor Relations</p>
+				<p>Press Releases</p>
+			</div>
+			<div className="footer-column">
+				<h3 className="footer-title">Make Money with Us</h3>
+				<p>Sell on Amazon</p>
+				<p>Sell Under Amazon Accelerator</p>
+				<p>Sell on Amazon Handmade</p>
+				<p>Advertise Your Products</p>
+				<p>Independently Publish with Us</p>
+				<p>Host an Amazon Hub</p>
+			</div>
+			<div className="footer-column">
+				<h3 className="footer-title">Amazon Payment Products</h3>
+				<p>Amazon.ca Rewards Mastercard</p>
+				<p>Shop with Points</p>
+				<p>Reload Your Balance</p>
+				<p>Gift Cards</p>
+				<p>Amazon Cash</p>
+			</div>
+			<div className="footer-column">
+				<h3 className="footer-title">Let Us Help You</h3>
+				<p>COVID-19 and Amazon</p>
+				<p>Shipping Rates & Policies</p>
+				<p>Amazon Prime</p>
+				<p>Returns Are Easy</p>
+				<p>Manage your Content and Devices</p>
+				<p>Customer Service</p>
+			</div>
+		</div>
+	</div>
+);
+export default Footer;
